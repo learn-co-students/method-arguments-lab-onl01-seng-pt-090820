7615c7f3b7a9105c21451d3c88e1f7de351cb4ed
candescent-tuple-8449 <singletarybush@gmail.com>
2020-09-11 17:20:41 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,7 @@
 #introduction_with_language
 
 def introduction(name, language)
-  puts "Hi, my name is #{name} 
-  puts and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction("Dan","Ember.js")
