869d17d728a51b73fcc787606ada640fcddd14d6
candescent-tuple-8449 <singletarybush@gmail.com>
2020-09-11 17:20:00 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+#introduction_with_language
+
+def introduction(name, language)
+  puts "Hi, my name is #{name} 
+  puts and I am learning to program in #{language}."
+end
+
+introduction("Dan","Ember.js")
+# > Hi, my name is Dan and I am learning to program in Ember.js
