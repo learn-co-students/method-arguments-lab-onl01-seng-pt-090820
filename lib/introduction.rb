3f5d0fe15a3b95e8aa4043921b4fa232741d1406
candescent-tuple-8449 <singletarybush@gmail.com>
2020-09-12 16:45:46 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,12 +4,8 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-introduction ("Josh")
 
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-
-introduction_with_language ("Dan", "Ember.js")
-# > Hi, my name is Dan and I am learning to program in Ember.js.
